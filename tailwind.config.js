7ee6606e15a1f97ea9ca821ec126d9372a863c71
jayfrankina <jayfrankina@oakland.edu>
2022-11-02 14:38:08 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./dist/**/*.{html,js}",
+    "/dist/Temp.html",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
